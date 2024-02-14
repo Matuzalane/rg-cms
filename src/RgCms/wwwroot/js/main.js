1b58b0c1e13727f05e6203f886f0dae759fb1133
Matuzalane <calebcastilhonunes@gmail.com>
2024-02-14 15:20:15 -0300

    Styling Home Swiper Slider.

--- src/RgCms/wwwroot/js/main.js
+++ src/RgCms/wwwroot/js/main.js
@@ -11,10 +11,13 @@ function initSwiper() {
         // Optional parameters
         direction: 'horizontal',
         loop: true,
+        autoplay: false,
 
         // If we need pagination
         pagination: {
             el: '.swiper-pagination',
+            clickable: true,
+            bulletElement: 'button',
         },
 
         // Navigation arrows
@@ -28,4 +31,43 @@ function initSwiper() {
             el: '.swiper-scrollbar',
         },
     });
+}
+
+/**
+ * Add a Event Listener for mause-hover 
+ * Style animation in the Top Line Links
+ */
+function topLinksHover() {
+    var elements = document.querySelectorAll("#top_links > li");
+
+    function handleHoverIn() {
+        var siblings = this.parentNode.children;
+        for (var i = 0; i < siblings.length; i++) {
+            if (siblings[i] !== this) {
+                siblings[i].style.opacity = 0.6;
+            }
+        }
+        var parentSiblings = this.parentNode.parentNode.children;
+        for (var j = 0; j < parentSiblings.length; j++) {
+            if (parentSiblings[j] !== this.parentNode) {
+                parentSiblings[j].style.opacity = 1;
+            }
+        }
+    }
+
+    function handleHoverOut() {
+        var siblings = this.parentNode.children;
+        for (var i = 0; i < siblings.length; i++) {
+            siblings[i].style.opacity = 1;
+        }
+        var parentSiblings = this.parentNode.parentNode.children;
+        for (var j = 0; j < parentSiblings.length; j++) {
+            parentSiblings[j].style.opacity = 1;
+        }
+    }
+
+    for (var k = 0; k < elements.length; k++) {
+        elements[k].addEventListener("mouseenter", handleHoverIn);
+        elements[k].addEventListener("mouseleave", handleHoverOut);
+    }
 }
